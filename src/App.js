00e1dd461f59fd188bf9902ad2ebf46f96b40cde
Julian <julianr.nicolas@gmail.com>
2022-05-09 15:22:05 -0300

    entregaprimera

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+Mi primera App de Ecommerce
+    </div>
+  );
+}
+
+export default App;
---
+++ src/newcomponent.js
@@ -0,0 +1,9 @@
+function NewComponent (){
+    return(
+        <div>
+        <h2>
+            Esto es un subtitulo
+        </h2>
+        </div>
+    )
+}
